410a3c0ac8a4058ab2634409aa77918b4fb50d57
Stan <stanislas.vindry@epita.fr>
2026-02-20 03:36:48 -0500

    updated what we need to do on the receive part

---
+++ intermediate_part/BackEnd/receive.py
@@ -0,0 +1,8 @@
+## Receive part is not needed right now as
+# we are using a prototype and we are juste moving
+# file inside one folder and not
+# between multiple system.
+
+
+#The only thing needed to be done is to get all the "parts" in one folder
+# 
